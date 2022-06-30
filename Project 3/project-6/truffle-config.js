6262b892a4ffb847084502ea9629ea11bc7832b5
Diego Sariol <45079960+sariold@users.noreply.github.com>
2022-06-29 22:51:29 -0500

    Project 3, coffee bean supply chain (WIP)

---
+++ Project 3/project-6/truffle-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	networks: {
+		development: {
+			host: "127.0.0.1",
+			port: 7545,
+			network_id: "*", // Match any network id
+			websockets: true,
+		},
+	},
+	compilers: {
+		solc: {
+			version: "0.8.1", // Fetch exact version from solc-bin (default: truffle's version)
+			// docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+			// settings: {          // See the solidity docs for advice about optimization and evmVersion
+			//  optimizer: {
+			//    enabled: false,
+			//    runs: 200
+			//  },
+			//  evmVersion: "byzantium"
+			// }
+		},
+	},
+};
